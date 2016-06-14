b5026a2510c9d66acb172f4ce58abb4c022cd58d
root <root@58f198d91948>
2016-06-14 05:57:18 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,10 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "ParcelTracking",
+  "homePage" : "Main",
+  "name" : "parceltracking",
+  "platformType" : "WEB",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
