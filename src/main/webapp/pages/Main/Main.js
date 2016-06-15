828eb28129ac2cbaaf833b40ba998ca5b7e6419c
manishchakravarty null <manish.chakravarty@wavemaker.com>
2016-06-15 07:02:25 +0000

    Google Maps integrated.

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,17 +1,50 @@
-Application.$controller("MainPageController", ["$scope", function ($scope) {
+Application.$controller("MainPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
+
+        // Constants
+        var GOOGLE_MAPS_API_KEY = "AIzaSyBzs_6EsdrgAmboMDgXKFm0_Iv_78EE-_Y";
+
+
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
          * $scope.Variables.loggedInUser.getData()
-         * 
+         *
          * widgets can be accessed through '$scope.Widgets' property here
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
          */
+
+
+
+        // method to intialize a Map
+        var map;
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('gMapsDiv'), {
+                center: {
+                    lat: -34.397,
+                    lng: 150.644
+                },
+                zoom: 8
+            });
+        }
+
+        // method to load script, after all WM scripts have loaded. Uses JQuery
+        function loadScript(scriptUrl) {
+            console.log("Starting load of: " + scriptUrl)
+            $.getScript(scriptUrl, function() {
+                console.log("Script loaded successfully")
+                initMap()
+            })
+
+        }
+
+        // Load Google Maps with given API KEY. Call initMap() method once loaded. 
+        loadScript("https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_MAPS_API_KEY);
     };
 
 }]);
